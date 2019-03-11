fcc06f3f1c79174d3be22a05f060e75cb7522c54
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-11 23:02:07 +0100

    Database setup

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('', views.info, name='info'),
     # path('index2', views.index, name='index2'),
 ]
