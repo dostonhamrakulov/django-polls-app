c87e935ca3840a420e8a891e25aeac2d25a12fb6
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-14 22:13:41 +0100

    Http load

--- polls/urls.py
+++ polls/urls.py
@@ -14,7 +14,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.info, name="info"),
+    # path('', views.info, name="info"),
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
