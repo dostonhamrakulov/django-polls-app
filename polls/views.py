9afe22694ce69dbbc106f24969ad7076f508b4b9
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-14 22:00:51 +0100

    Creating views

--- polls/views.py
+++ polls/views.py
@@ -9,4 +9,14 @@ def index(request):
 def info(request):
     return HttpResponse("This is second")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 
