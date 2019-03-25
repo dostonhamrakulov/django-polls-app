d3a65e89e9d5695f9945081572d216ba58374c73
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-25 22:50:26 +0100

    keraksiz narsalar

--- polls/urls.py
+++ polls/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path
-#
-# from . import views
-#
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('', views.info, name='info'),
-#     # path('index2', views.index, name='index2'),
-# ]
-
-
 from django.urls import path
 
 from . import views
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,3 @@
-# from django.shortcuts import render
-
-# from django.template import loader
-#
-# from .models import Question
-#
-#
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
